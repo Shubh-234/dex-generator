97e4dc6c00c66a151cfc7c145083f48476bcf754
Shubh-234 <shubhankarbhanot@gmail.com>
2025-07-26 02:59:43 +0530

    added nodemon and created entry point for the backend project

---
+++ src/index.ts
@@ -0,0 +1,14 @@
+const express = require("express");
+const app = express();
+const dotenv = require("dotenv");
+const path = require("path");
+dotenv.config({path : path.resolve(__dirname, "../.env")}   );
+
+app.use(express.json());
+
+const port = process.env.PORT;
+
+
+app.listen(port, ()=> {
+    console.log(`Server is running on port ${port}`);
+})
