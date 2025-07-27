5e88b5b8bbbafaa811a3c5d3b3e21fe1411d603b
Shubh-234 <shubhankarbhanot@gmail.com>
2025-07-27 15:35:30 +0530

    implemented rate limiter

--- src/routes/tokens.ts
+++ src/routes/tokens.ts
@@ -1,10 +1,20 @@
 import express from 'express';
 import { aggregateTokens } from '../aggregator';
+import rateLimit from 'express-rate-limit';
 
 const router = express.Router();
 
+const rateLimiter = rateLimit({
+    windowMs: 60 * 1000 *30,
+    max: 10, // limit each IP to 50 requests per 30 minutes
+    message: {
+        success: false,
+        message: "Too many requests, please try again later."
+    }
+})
 
-router.get('/tokens', async (req:any, res:any) => {
+
+router.get('/tokens',rateLimiter, async (req:any, res:any) => {
     try {
         const tokens = await aggregateTokens();
         return res.status(200).json({
